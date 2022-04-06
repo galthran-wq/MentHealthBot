f099ff76f766f9d1da2008286b8342ee95139933
Ivan Chekanov <is.2003.01@gmail.com>
2022-04-06 21:19:13 +0300

    :art: Renamed variable

--- src/utils/find_appeals_by_doctor_id.py
+++ src/utils/find_appeals_by_doctor_id.py
@@ -3,10 +3,10 @@ from models import Appeal
 
 
 def find_appeals_by_doctor_id(therapist: User) -> list[str]:
-    data = Appeal.select().where(Appeal.therapist == therapist.id)
-    if data:
+    db_rows = Appeal.select().where(Appeal.therapist == therapist.id)
+    if db_rows:
         appeals = []
-        for appeal in data:
+        for appeal in db_rows:
             user = User.get_by_id(appeal.patient_id)
             appeals.append(f"{user.first_name} {user.last_name}")
         return appeals
