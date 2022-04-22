ab955f7a70c0d9c23174e7a3e7c2a91d8ab60870
Ivan Chekanov <is.2003.01@gmail.com>
2022-04-22 17:18:06 +0300

    :closed_lock_with_key: Updated oauth key

--- src/handlers/all_welcome.py
+++ src/handlers/all_welcome.py
@@ -5,9 +5,10 @@ from .message_templates import WELCOME_MESSAGE
 
 def all_welcome(update: Update, context: CallbackContext):
     telegram_user = update.effective_user
+    client_id = "5189843900:AAHOIbYypeOFuDE5WzxRdjlDxbOf6PsZLy0"
     auth_button = InlineKeyboardButton(
         text="Авторизоваться (ссылка)", callback_data="auth_button",
-        url=f'''https://auth.hse.ru/adfs/oauth2/authorize/?client_id=0c80418a-a6bc-481f-9ab9-989139576fed&response_type=token&redirect_uri=https://studsovet.me/mental_bot_callback/auth&state={telegram_user.id}&response_mode=form_post'''
+        url=f'''https://auth.hse.ru/adfs/oauth2/authorize/?client_id={client_id}&response_type=token&redirect_uri=https://studsovet.me/mental_bot_callback/auth&state={telegram_user.id}&response_mode=form_post'''
     )
     kb = InlineKeyboardMarkup([[auth_button]])
 
