55d6f5377d2f91e0dce1b91ffa52b1c735cdf865
povle <pasha@blinov.co>
2022-04-27 02:09:41 +0300

    peeweedbevolve doesn't work :(

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,6 @@
-import peeweedbevolve
 from .user import User
 from .appeal import Appeal
 from .message import Message
 from .base_model import db_client
 
-db_client.evolve([User, Appeal, Message])
+db_client.create_tables([User, Appeal, Message])
