c2de59b107574e8b1b8ca8a40fe1f84639dd392b
root <>
2022-04-28 23:33:03 +0200

    :bug: Fixed query

--- src/utils/get_new_appeals.py
+++ src/utils/get_new_appeals.py
@@ -2,16 +2,12 @@ from models import Appeal, User, connection_types
 
 
 def get_new_appeals() -> list[(str, int)]:
-    db_strings = Appeal.select().where((Appeal.therapist is None) & (Appeal.active) & (not Appeal.connection_type is None))
+    db_strings = Appeal.select().where((Appeal.therapist == None) & (Appeal.active == True) & (Appeal.connection_type != None))
     if db_strings:
         new_appeals = []
         for appeal in db_strings:
             user = User.get_by_id(appeal.patient_id)
-            try:
-                conn = connection_types.CONNECTION_TYPES[appeal.connection_type]
-            except KeyError:
-                conn = "способ не указан"
+            conn = connection_types.CONNECTION_TYPES[appeal.connection_type]
             new_appeals.append((f"{user.first_name} {user.last_name}, {conn}", appeal.id))
         return new_appeals
-    else:
-        return []
+    return []
