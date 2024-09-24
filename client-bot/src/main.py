64a32ea0f330c43ba3dd1b9d03ec024f9aa16d92
galthran-wq <levmorozov900@gmail.com>
2024-09-24 19:01:50 +0300

    remove webhooks

--- client-bot/src/main.py
+++ client-bot/src/main.py
@@ -6,8 +6,6 @@ from notifications.new_appeals import new_appeals_notify
 
 if __name__ == '__main__':
     token = config("BOT_TOKEN")
-    webhook_url = config("WEBHOOK_DOMAIN")
-    webhook_port = config("WEBHOOK_PORT")
     prod = config("PROD")
 
     updater = Updater(token=token)
@@ -22,15 +20,4 @@ if __name__ == '__main__':
         time(hour=8-3)
     )
 
-    if prod == "PROD":
-        updater.start_webhook(
-            listen="0.0.0.0",
-            port=webhook_port,
-            url_path=token,
-            webhook_url=webhook_url + token,
-            max_connections=100,
-        )
-    else:
-        updater.start_polling()
-
-    updater.idle()
+    updater.start_polling()
