6bd81b96ec16725b57f3eb26e04b862af2bf2f82
galthran-wq <levmorozov900@gmail.com>
2024-09-24 21:09:34 +0300

    wip

--- admin-bot/src/utils/find_user_by_telegram_user.py
+++ admin-bot/src/utils/find_user_by_telegram_user.py
@@ -1,10 +1,10 @@
 from typing import Optional
 from models import User as BotUser
 from telegram import User
-from .user_by_telegram_id_query import user_by_telegram_id_query
+from .user_by_telegram_id_query import user_by_telegram_id_query, user_by_telegram_username_query
 
 
 def find_user_by_telegram_user(telegram_user: User) -> Optional[BotUser]:
-    if user_by_telegram_id_query(telegram_user.id).exists():
-        return user_by_telegram_id_query(telegram_user.id).get()
+    if user_by_telegram_username_query(telegram_user.username).exists():
+        return user_by_telegram_username_query(telegram_user.username).get()
     return None
--- admin-bot/src/utils/user_by_telegram_id_query.py
+++ admin-bot/src/utils/user_by_telegram_id_query.py
@@ -4,3 +4,6 @@ from models import User
 
 def user_by_telegram_id_query(incoming_id: int) -> Node:
     return User.select().where(User.telegram_id == incoming_id)
+
+def user_by_telegram_username_query(incoming_username: int) -> Node:
+    return User.select().where(User.telegram_username == incoming_username)
