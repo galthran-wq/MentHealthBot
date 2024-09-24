9a2fdf28652a3149be71a1837d6b17dea050d30f
galthran-wq <levmorozov900@gmail.com>
2024-09-24 20:45:05 +0300

    wip

--- admin-bot/src/utils/create_admin_by_email.py
+++ admin-bot/src/utils/create_admin_by_email.py
@@ -1,4 +1,6 @@
 from models import User
+from random import randint
+from peewee import IntegrityError
 
 def create_admin_by_email(email: str):
     new_user = User(
@@ -10,9 +12,15 @@ def create_admin_by_email(email: str):
 
 
 def create_admin_by_telegram_username(telegram_username: str):
-    new_user = User(
-        telegram_id = -1,
-        admin=True,
-        telegram_username=telegram_username
-    )
-    new_user.save()
+    for i in [randint(-10000, -1) for _ in range(3)]:
+        try:
+            new_user = User(
+                telegram_id = i,
+                admin=True,
+                telegram_username=telegram_username
+            )
+            new_user.save()
+        except IntegrityError:
+            pass
+        else:
+            break
