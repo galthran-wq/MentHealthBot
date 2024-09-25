694342e06975277cdf982e3f4fc8ab75037e3a77
galthran-wq <levmorozov900@gmail.com>
2024-09-25 18:46:22 +0300

    wip

--- client-bot/src/utils/find_user.py
+++ client-bot/src/utils/find_user.py
@@ -1,10 +1,20 @@
 from typing import Optional
 from models import User as BotUser
 from telegram import User
-from .user_by_telegram_id_query import user_by_telegram_id_query
+from .user_by_telegram_id_query import user_by_telegram_id_query, user_by_telegram_username_query
 
 
 def find_user(telegram_user: User) -> Optional[BotUser]:
+    return find_user_by_telegram_username(telegram_user)
+
+
+def find_user_by_telegram_id(telegram_user: User) -> Optional[BotUser]:
     if user_by_telegram_id_query(telegram_user.id).exists():
         return user_by_telegram_id_query(telegram_user.id).get()
+    return None
+
+
+def find_user_by_telegram_username(telegram_user: User) -> Optional[BotUser]:
+    if user_by_telegram_username_query(telegram_user.username).exists():
+        return user_by_telegram_username_query(telegram_user.username).get()
     return None
--- client-bot/src/utils/user_by_telegram_id_query.py
+++ client-bot/src/utils/user_by_telegram_id_query.py
@@ -3,3 +3,7 @@ from models import User
 
 def user_by_telegram_id_query(incoming_id: int) -> Node:
     return User.select().where(User.telegram_id == incoming_id)
+
+
+def user_by_telegram_username_query(incoming_username: int) -> Node:
+    return User.select().where(User.telegram_username == incoming_username)
