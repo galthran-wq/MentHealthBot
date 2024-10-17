986ce59b455e31a4bf6797fc5dac89b86039b700
galthran-wq <levmorozov900@gmail.com>
2024-10-18 01:13:02 +0300

    wip

--- admin-bot/src/utils/delete_user_by_telegram_username.py
+++ admin-bot/src/utils/delete_user_by_telegram_username.py
@@ -2,8 +2,7 @@ from utils.user_by_telegram_username import user_by_telegram_username
 
 def delete_user_by_telegram_username(telegram_username: str) -> bool:
     user_ref = user_by_telegram_username(telegram_username)
-    
-    if user_ref.get().exists:
+    if user_ref:
         user_ref.delete()
         return True
     else:
