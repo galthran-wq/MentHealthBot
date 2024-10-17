ffaecb7dc859306e04f5017b7a31ab64a0b28acf
galthran-wq <levmorozov900@gmail.com>
2024-10-18 00:28:04 +0300

    logging in new_appeals

--- client-bot/src/notifications/new_appeals.py
+++ client-bot/src/notifications/new_appeals.py
@@ -1,3 +1,4 @@
+import logging
 from telegram import ParseMode
 from telegram.ext import CallbackContext
 from utils.get_all_doctors import get_all_doctors
@@ -6,13 +7,22 @@ from .message_templates.new_appeals_message import NEW_APPEALS_MESSAGE
 
 
 def new_appeals_notify(context: CallbackContext):
+    logging.info("Starting new appeals notification process")
     new_appeals = get_new_appeals()
     number_of_appeals = len(new_appeals)
+    logging.info(f"Found {number_of_appeals} new appeals")
     if number_of_appeals:
         doctor_ids = get_all_doctors()
+        logging.info(f"Sending notifications to {len(doctor_ids)} doctors")
         for doctor in doctor_ids:
-            context.bot.send_message(
-                chat_id=doctor,
-                text=NEW_APPEALS_MESSAGE.format(number_of_appeals),
-                parse_mode=ParseMode.HTML
-            )
+            try:
+                context.bot.send_message(
+                    chat_id=doctor,
+                    text=NEW_APPEALS_MESSAGE.format(number_of_appeals),
+                    parse_mode=ParseMode.HTML
+                )
+                logging.info(f"Notification sent to doctor with ID: {doctor}")
+            except Exception as e:
+                logging.error(f"Failed to send notification to doctor with ID: {doctor}. Error: {str(e)}")
+    else:
+        logging.info("No new appeals found, skipping notifications")
